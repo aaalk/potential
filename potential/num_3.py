c6028889b296ce96d8b6e026df0b987ca610a599
aaalk <115035998+aaalk@users.noreply.github.com>
2024-04-01 12:43:46 +0300

    Update num_3.py

--- potential/num_3.py
+++ potential/num_3.py
@@ -2,21 +2,13 @@
 import csv
 
 with open('history_mirror.csv', encoding="utf8") as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-    data = sorted(reader, key=lambda x: x['date'])
-a = input()
-# ввод имени и отчества через пробел, записываем в переменную а
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')  # открытие файла в переменную reader
+    answer = list(reader)[1:]
+    name = input()  # ввод данных
+    while name != 'stop':
+        n = 0
+        for row in answer:
+            if name in row[1]:
+                print(f'Предсказание для {row[1].split()[0]} {row[1].split()[1][0]}.{row[1].split()[2][0]}. - {row[2]}')  # вывод предсказания
 
-while ('stop' not in a):
-    for el in data:
-        if a in el['username']:
-            # итерируемся по каждой строчке, если такое имя и отчество есть в таблице, то с помощью метода split раделяем ячейку с фио на отдельные фамилию,  имя и отчество, записываем в переменные
-
-            surname, name, fath = el["username"].split()
-            print(f'Предсказание для {surname} {name[0]}.{fath[0]}. - {el["verdict"]}')
-            # вывод через f string как просят в задании
-            break
-    else:
-        print('Вас не нашлось в записях')
-    a = input()
-    # ввод данных повторно
+        name = input()  # ввод данных повторно
